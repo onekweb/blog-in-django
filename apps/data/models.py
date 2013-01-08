28163eedc6afa506ac831bc61a8d7d1c7bc5867c
Mario Onekanda <mario.onekanda@gmail.com>
2013-01-08 20:35:17 +0100

    changes 2013

--- apps/data/models.py
+++ apps/data/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from apps.data.managers import EntryManager
 
 class Entry(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
@@ -6,6 +7,8 @@ class Entry(models.Model):
 	title   = models.CharField(max_length=64)
 	text    = models.TextField()
 	published = models.BooleanField(db_index=True, default=True)
+	objects = EntryManager()
+
 
 	def __unicode__(self):
 		return u"%s - %s" % (self.title, self.created)
--- apps/homepage/views.py
+++ apps/homepage/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render_to_response
 from apps.data.models import Entry
 
 def index(request):
-	entries = Entry.objects.filter(published=True).order_by('-id')
+	entries = Entry.objects.published_entries().order_by('-id')
 	ctx = { 'entries':entries }
 	return render_to_response('homepage/index.html', ctx)
