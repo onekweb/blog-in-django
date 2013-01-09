5605c00255a12ee78afb945aa867829928fc5230
Mario Onekanda <mario.onekanda@gmail.com>
2013-01-09 18:33:31 +0100

    removed apps directory

--- apps/data/managers.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class EntryManager(models.Manager):
-	def published_entries(self):
-		return self.model.objects.filter(published=True)
--- apps/homepage/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-	(r'^$', 'apps.homepage.views.index'),
-)
